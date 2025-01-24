9591a68a4004c3b166c14f9c358a2655d6300366
valitovgaziz <valitovgaziz@gmail.com>
2025-01-25 00:41:40 +0500

    clean start project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="main">
+    <h2>This is welcome page</h2>
+  </div>
 </template>
+
+
+<style>
+@media (min-width: 1024px) {
+  .main {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
