38fb6e0de7d575d09ba292b73e8ee355caa00bf0
valitovgaziz <valitovgaziz@gmail.com>
2025-01-28 00:59:06 +0500

    add component menu.

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,17 +1,7 @@
 <template>
     <div class="header">
         <router-link to="/about"><img src="./images/logo150x150.png" alt="logo">ООО "ИКЦ Ял Арба"</router-link>
-        <div class="menu">
-            <img src="./images/icons/menu_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="menu">
-            <ul>
-                <li><router-link to="/profile">Профиль</router-link></li>
-                <li>Настройки</li>
-                <li><router-link to="/about">О нас</router-link></li>
-                <li><router-link to="/support">Написать в техпотдержку</router-link></li>
-                <li> <router-link to="/feetback">Оставить отзыв</router-link></li>
-                <li>Вход/Выход</li>
-            </ul>
-        </div>
+        <Menu />
     </div>
 </template>
 
@@ -25,36 +15,20 @@
     height: 2rem;
 }
 
-ul {
-    margin: 0 0.5rem 0.5rem 0;
-}
-
-li {
-    margin: 3px 0;
-    border: solid 1px black;
-    border-radius: 1rem;
-    padding: 0 0.5rem;
-}
-
-li:hover {
-    cursor: pointer;
-    background-color: rgb(153, 204, 204);
-    border-radius: 1rem;
-}
-
 img {
     height: 1.5rem;
 }
 
-.menu {
-    background-color: darkgreen;
-    width: fit-content;
-}
 </style>
 
 
 <script>
+import Menu from './header/menu.vue';
+
 export default {
+    components: {
+        Menu,
+    },
     name: 'header',
 }
 </script>
---
+++ src/components/header/menu.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="menu">
+        <img src="./images/icons/menu_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="menu">
+        <ul>
+            <li><router-link to="/profile">Профиль</router-link></li>
+            <li>Настройки</li>
+            <li><router-link to="/about">О нас</router-link></li>
+            <li><router-link to="/support">Написать в техпотдержку</router-link></li>
+            <li> <router-link to="/feetback">Оставить отзыв</router-link></li>
+            <li>Вход/Выход</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.menu {
+    background-color: darkgreen;
+    width: fit-content;
+}
+
+ul {
+    margin: 0 0.5rem 0.5rem 0;
+}
+
+li {
+    margin: 3px 0;
+    border: solid 1px black;
+    border-radius: 1rem;
+    padding: 0 0.5rem;
+}
+
+li:hover {
+    cursor: pointer;
+    background-color: rgb(153, 204, 204);
+    border-radius: 1rem;
+}
+</style>
+
+<script>
+export default {
+    name: 'menu',
+}
+</script>
