980b06b2932ff3114557c66add2206f76dcc2c2a
valitovgaziz <valitovgaziz@gmail.com>
2025-02-06 08:39:10 +0500

    Разбил на компоненты header.

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="header">
-        <router-link to="/about" id="rl-about"><img src="../images/logo150x150.png" alt="logo"></router-link>
-        <router-link to="/filosofy" id="rl-filosofy">Trip*Freedom*Adventure</router-link>
+        <Logo />
+        <TFA />
         <Menu />
     </div>
 </template>
@@ -14,56 +14,25 @@
     margin: 0;
     padding: 0;
     border-radius: 1rem;
-    background-color: aquamarine;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
     height: 3rem;
 }
-
-#rl-about {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-items: center;
-    border-radius: 50%;
-    padding: 1rem;  
-    height: 1rem;
-    width: 1rem;
-}
-
-#rl-about:hover {
-    cursor: pointer;
-    background-color: rgb(85, 117, 117);
-}
-
-img {
-    height: 1.5rem;
-    padding: 0 1rem 0 0;
-}
-
-#rl-filosofy {
-    all: unset;
-    box-shadow: 1px 2px 3px rgb(63, 145, 130);
-    border-radius: 1rem;
-    padding: 0.5rem;
-}
-
-#rl-filosofy:hover {
-    cursor: pointer;
-    background-color: rgb(143, 202, 202);
-    box-shadow: 2px 3px 4px rgb(64,146, 132);
-}
 </style>
 
 
 <script>
 import Menu from './menu.vue';
+import Logo from './logo-rl-about.vue';
+import TFA from './tripFreedomAdventure.vue';
 
 export default {
     components: {
         Menu,
+        Logo,
+        TFA,
     },
     name: 'header',
 }
---
+++ src/components/header/logo-rl-about.vue
@@ -0,0 +1,32 @@
+<template>
+    <router-link to="/about" id="rl-about"><img src="../images/logo150x150.png" alt="logo"></router-link>
+</template>
+
+<style scoped>
+#rl-about {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    padding: 1rem;
+    height: 1rem;
+    width: 1rem;
+}
+
+#rl-about:hover {
+    cursor: pointer;
+    background-color: rgb(78, 102, 94);
+}
+
+img {
+    height: 2rem;
+    box-shadow: 1px 2px 3px rgb(117, 194, 208);
+    border-radius: 50%;
+    padding: 0.4rem;
+}
+</style>
+
+<script>
+
+</script>
