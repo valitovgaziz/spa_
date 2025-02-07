c66296ae55cd8843c1ab5fdce4e8ab49f8579214
valitovgaziz <valitovgaziz@gmail.com>
2025-02-08 02:41:43 +0500

    Добавил сервер для отзывов на ноде, настроил страницу оставления отзывов.

---
+++ servers/feedbackserver.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+const pool = new Pool({
+	user: 'postgres',
+	host: 'localhost',
+	database: 'postgres',
+	password: 'postgres',
+	port: 5432,
+});
+
+app.get('/reviews', async(req, res) => {
+	try {
+		const result = await pool.query('SELECT * FROM reviews;');
+		res.json(result.rows);
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Server error");
+	}
+});
+
+app.post('/reviews', async(req, res) => {
+	const { text } = req.body;
+	try {
+		const result = await pool.query('INSERT INTO reviews (text) VALUES ($1) RETURNING *', [text]);
+		res.status(201).json(result.rows[0]);
+	} catch (err) {
+		console.log(err);
+		res.status(500).send("Server error");
+	}
+});
+
+
+app.listen(port, '0.0.0.0', () => {
+	console.log("Server is running on port ${port}");
+});
