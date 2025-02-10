a6323c3f1067ca60af417ec3f58f0b4676f24817
valitovgaziz <valitovgaziz@gmail.com>
2025-02-10 17:44:36 +0500

    Добавил шрифты на проект

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue';
 import SPA_VUE_App from './App.vue';
 import appRouter from './router/index.js';
 import i18n from './locales/i18n.js';
+import './assets/fonts.css';
 
 const spaAppVue3 = createApp(SPA_VUE_App);
 
